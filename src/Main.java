edcae7da66862aa142074a4da52dc9859c05a811
Lebedev Leonid <leoTop705@gmail.com>
2026-09-19 21:08:22 +0300

    переработка и дальнейшее совершенствование

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
-
+/* задание 15 используя список, удалить из текста заданный набор букв*/
 void main(String[] args) {
     String text_del;
     Scanner in = new Scanner(System.in);
     System.out.print("Input text: ");
     String text = in.nextLine();
     System.out.print("Input how much letter do you want to dell: ");
-    int num = Integer.parseInt(in.nextLine());
-    for (int i = 0; i <= num; i++){
+    int num = in.nextInt();
+    for (int i = 0; i <= num; i++) {
         System.out.print("Input letter: ");
-        String letter = in.nextLine();
-        Node.pushFront(letter);
+        String letter = in.next();
+//        if (Node.head == null){
+//            Node head = new Node(letter.charAt(0));
+//        }
+        Node.pushBack(letter.charAt(0));
     }
-    for (int i = 0; i <= text.length(); i++) {
+
+    while (Node.giveData() != null){
+        String text_del = text.replace(буква, );
 
     }
+//    for (int i = 0; i <= text.length(); i++) {
+//        Node current = Node.head;
+//        while(current != null){
+//            char let = text.charAt(i);
+//            if (let == Node.find(current)){
+//                text.replace
+//            }
+//
+//        }
+//    }
 
     System.out.print(text_del);
 }
--- src/Node.java
+++ src/Node.java
@@ -9,17 +9,7 @@ public class Node {
 
     Node head = null;
 
-    public static char printList(Node head) {
-        Node current = head;
-
-        while (current != null) {
-            System.out.print(current.data + " ");
-            current = current.next;
-        }
-    }
-
-
-    public static Node find(Node head, int value) {
+    public static Node find(Node head, char value) {
         Node current = head;
 
         while (current != null) {
@@ -32,10 +22,21 @@ public class Node {
         return null;
     }
 
-    public static Node pushFront(Node head, char value) {
+    public static void pushBack(char value) {
         Node newNode = new Node(value);
 
-        newNode.next = head;
-        return newNode;
+        if (newNode.head == null) {
+            newNode.head = newNode;
+        }
+        newNode.next = newNode;
     }
+
+    public Node giveData() {
+        Node current = head;
+        while (current != null){
+            return current;
+        }
+        return null;
+    }
+
 }
